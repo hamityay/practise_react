356f50406cdd7c901810f72eddcc214cfa8212fd
hamityay <hamityay@hotmail.com>
2019-12-31 18:27:30 +0300

    Convert defined function to arrow func.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-function Header() {
+const Header = () => {
   return (
     <header>
-      <h1>Scoreboard</h1>
-      <span className="stats">Players 1</span>
-    </header>
+    <h1>Scoreboard</h1>
+    <span className="stats">Players 1</span>
+  </header>
   );
-}
+};
 
 ReactDOM.render(
   <Header />,
