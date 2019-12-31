fc311cb295ca8958e884b48278fa4e033556fa84
hamityay <hamityay@hotmail.com>
2019-12-31 18:36:36 +0300

    Create Player component.

--- app.js
+++ app.js
@@ -5,9 +5,30 @@ const Header = () => {
     <span className="stats">Players 1</span>
   </header>
   );
-};
+}
+const Counter = () => {
+  return (
+    
+  );
+}
+
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        HMT
+      </span>
+
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">10</span>
+        <button className="counter-action increment"> + </button>
+      </div>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <Header />,
+  <Player />,
   document.getElementById('root')
 );
