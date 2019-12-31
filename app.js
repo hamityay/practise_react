989fe6e66ac2a959215c6256df2c494d75b0132e
hamityay <hamityay@hotmail.com>
2019-12-31 17:49:22 +0300

    Crate a simple react element.

---
+++ app.js
@@ -0,0 +1,8 @@
+const title = React.createElement(
+  'h1',
+  { id: 'main-title', title: 'This is a main title.' },
+  'My first React Element!'
+);
+
+// <h1 id="main-title" title="This is a main title.">My first React Element!</h1>
+console.log('title: ', title);
