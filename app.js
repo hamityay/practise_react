785afffcfc5cc42a250cdd1957e6b62cc72c0820
hamityay <hamityay@hotmail.com>
2019-12-31 18:04:49 +0300

    More on using JSX #2

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const title = <h1>My first React Element!</h1>;
-
-const desc = <p>I just learned how to create a React node and render it into Dom.</p>;
+const title = 'My first React Element!';
+const desc = 'I just learned how to create a React node and render it into Dom.';
 
 const header = (
   <header>
-    { title }
-    { desc }
+    <h1>{ title }</h1>
+    <p calssName="main-desc">{ desc }</p>
   </header>
 );
 
