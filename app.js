e0563ff07e4767d41fa8463f02a514a3e9eeec88
hamityay <hamityay@hotmail.com>
2019-12-31 22:31:28 +0300

    Add event handler to increase counter and bind to use this inside of the func.

--- app.js
+++ app.js
@@ -31,17 +31,22 @@ const Header = (props) => {
 }
 
 class Counter extends React.Component {
-
   state = {
     score: 0
   };
+
+  incrementScore() {
+    this.setState({
+      score: this.state.score + 1
+    });
+  }
 
   render() {
     return(
       <div className="counter">
         <button className="counter-action decrement"> - </button>
         <span className="counter-score">{ this.state.score }</span>
-        <button className="counter-action increment"> + </button>
+        <button className="counter-action increment" onClick={ this.incrementScore.bind(this) }> + </button>
       </div>
     );
   }
