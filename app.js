28bd377c0bc7c69bb2d1572e459f8198e8e1507d
hamityay <hamityay@hotmail.com>
2019-12-31 17:55:30 +0300

    Render react elements.

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ const title = React.createElement(
   'My first React Element!'
 );
 
-ReactDOM.render(
+const desc = React.createElement(
+  'p',
+  null,
+  'I just learned how to create a React node and render it into DOM.'
+);
+
+const header = React.createElement(
+  'header',
+  null,
   title,
+  desc
+);
+
+ReactDOM.render(
+  header,
   document.getElementById('root')
 );
